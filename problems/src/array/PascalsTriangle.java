ca12faa6c39d5f7fd7ae9b83e1d864810743014d
twb0223 <twb0223@gmail.com>
2017-07-05 14:38:32 +0800

    使用公式法替代

--- problems/src/array/PascalsTriangle.java
+++ problems/src/array/PascalsTriangle.java
@@ -20,40 +20,11 @@ public class PascalsTriangle
 {
     public static void main(String[] args) throws Exception
     {
-        System.out.println(new PascalsTriangle().getRow2(3));
+        System.out.println(new PascalsTriangle().getRow(3));
     }
+
 
     public List<Integer> getRow(int rowIndex)
-    {
-        int k  = rowIndex;
-        if(k == 0)
-            return Arrays.asList(1);
-        else if(k == 1)
-            return Arrays.asList(1, 1);
-        else if (k == 2)
-            return Arrays.asList(1, 2, 1);
-        List<Integer> result = new ArrayList<>();
-        result.add(2);
-        k = k - 2;
-        int p, c;
-        while(k-- > 0)
-        {
-            p = 1;
-            int i = 0;
-            for(int l = result.size(); i < l; i ++)
-            {
-                c = result.get(i);
-                result.set(i, p + c);
-                p = c;
-            }
-            result.add(p + 1);
-        }
-        result.add(0, 1);
-        result.add(1);
-        return result;
-    }
-
-    public List<Integer> getRow2(int rowIndex)
     {
         // rowIndex是0开始的，我们将它加1，得到k
         int k = rowIndex + 1;
